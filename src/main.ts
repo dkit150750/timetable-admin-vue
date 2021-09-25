1436412bf91226457d37ae471510a3847ea9518a
dkit150750 <dkit150750gmail.com>
2021-09-26 01:25:46 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@
 import { ViteSSG } from 'vite-ssg';
 import generatedRoutes from 'virtual:generated-pages';
 import { setupLayouts } from 'virtual:generated-layouts';
-import { useUserStore } from '~/stores/user';
 import App from './App.vue';
 
 import '~/assets/styles/main.css';
@@ -15,11 +14,4 @@ const routes = setupLayouts(generatedRoutes);
 export const createApp = ViteSSG(App, { routes }, (context) => {
 	// install all modules under `modules/`
 	Object.values(import.meta.globEager('./modules/*.ts')).map((index) => index.install?.(context));
-	context.router.beforeEach((to) => {
-		const userStore = useUserStore();
-		if (to.name === 'all') return true;
-		if (to.name !== 'login' && !userStore.user.data) return '/login';
-		if (to.name === 'login' && userStore.user.data) return '/';
-		return true;
-	});
 });
--- src/modules/pinia.ts
+++ src/modules/pinia.ts
@@ -1,9 +1,10 @@
 import { createPinia } from 'pinia';
 import { UserModule } from '~/types';
+import { useUserStore } from '~/stores/user';
 
 // Setup Pinia
 // https://pinia.esm.dev/
-export const install: UserModule = ({ isClient, initialState, app }) => {
+export const install: UserModule = ({ isClient, initialState, app, router }) => {
 	const pinia = createPinia();
 	app.use(pinia);
 	// Refer to
@@ -11,4 +12,11 @@ export const install: UserModule = ({ isClient, initialState, app }) => {
 	// for other serialization strategies.
 	if (isClient) pinia.state.value = initialState.pinia || {};
 	else initialState.pinia = pinia.state.value;
+	router.beforeEach((to) => {
+		const userStore = useUserStore(pinia);
+		if (to.name === 'all') return true;
+		if (to.name !== 'login' && !userStore.user.data) return '/login';
+		if (to.name === 'login' && userStore.user.data) return '/';
+		return true;
+	});
 };
