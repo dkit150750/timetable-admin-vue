6d6d1747e2cb7327bc856c865cc438640c554977
dkit150750 <dkit150750gmail.com>
2021-10-12 01:05:53 +0300

    page news

---
+++ src/pages/news.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="admin-news">
+		<News />
+	</div>
+</template>
+
+<script setup lang="ts">
+import News from '~/md/news.md';
+</script>
+
+<style>
+.admin-news {
+	padding: 1rem;
+}
+
+.admin-news h1 {
+	font-size: 22px;
+}
+
+.admin-news h2 {
+	font-size: 18px;
+}
+
+.admin-news h2 + p {
+	font-weight: 600;
+	font-size: 16px;
+}
+</style>
